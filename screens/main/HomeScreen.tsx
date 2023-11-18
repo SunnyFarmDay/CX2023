8cd31290cb0e602a2bf5f3533256f541e71c6f91
huenwat01 <winghuenwat@gmail.com>
2023-11-19 02:07:27 +0800

    added badges changes and modified cathay community text

--- screens/main/HomeScreen.tsx
+++ screens/main/HomeScreen.tsx
@@ -16,7 +16,7 @@ import {DataContext} from '../../store/Data';
 import Logout from '../../components/Logout';
 import Toast from 'react-native-toast-message';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Colors } from '../Constants';
+import {Colors} from '../Constants';
 
 import {PieChart} from 'react-native-chart-kit';
 import LinkButton from '../../components/LinkButton';
@@ -30,8 +30,6 @@ const chartConfig = {
   barPercentage: 0.5,
   useShadowColorFromDataset: false, // optional
 };
-
-
 
 const HomeScreen = ({navigation}) => {
   const Data = useContext(DataContext);
@@ -83,32 +81,108 @@ const HomeScreen = ({navigation}) => {
           <ScrollView
             style={{
               backgroundColor: 'ghostwhite',
-              height: Data.screenHeight - 210,
+              height: '100%',
               width: Data.screenWidth,
               position: 'absolute',
             }}
             showsVerticalScrollIndicator={false}>
-              <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 0, position: 'relative'}}>
-                <Image source={require('../../assets/HomeScreen/HomeScreen_background_1.jpg')} style={{width: '100%', aspectRatio: 9 / 20}} 
-          resizeMode="cover"/>
-                <TouchableOpacity style={{position: 'absolute', top: 10, right: 10, width: 50, height: 50, borderRadius: 25, backgroundColor: 'white'}} onPress={() => setOpenSideBar(true) } />
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: 0,
+                position: 'relative',
+              }}>
+              <Image
+                source={require('../../assets/HomeScreen/HomeScreen_background_1.jpg')}
+                style={{width: '100%', aspectRatio: 9 / 20}}
+                resizeMode="cover"
+              />
+              <TouchableOpacity
+                style={{
+                  position: 'absolute',
+                  top: 20,
+                  right: 5,
+                  width: 50,
+                  height: 50,
+                  borderRadius: 25,
+                }}
+                onPress={() => setOpenSideBar(true)}
+              />
 
-                <View style={{position: 'absolute', top: 510, left: 0, width: '100%', height: 215, backgroundColor: 'white', borderTopColor: 'grey', borderTopWidth: 1, borderBottomColor: 'grey', borderBottomWidth: 1}}>
-                <Text style={{fontSize: 22, fontWeight: 'semibold', color: Colors.cathayGold, position: 'absolute', top: 20, left: 20}}>Cathay Badge</Text>
-                <LinkButton title="More details →" onPress={() => navigation.navigate('Badge')} 
-                styles={{
-                  touch: {position: 'absolute', top: 14, left: 275, borderRadius: 10, padding: 10}, 
-                  buttonText: {color: Colors.cathayGreen, fontSize: 15, fontWeight: 'semibold'}}}/>
-                <Text style={{fontSize: 15, fontWeight: 'semibold', color: Colors.black, position: 'absolute', top: 60, left: 20}}>You have earned 3 badges</Text>
-                <Text style={{fontSize: 20, fontWeight: 'semibold', color: Colors.black, position: 'absolute', top: 105, left: 20}}>Let's get moving to get more badges!</Text>
+              <View
+                style={{
+                  position: 'absolute',
+                  top: 510,
+                  left: 0,
+                  width: '100%',
+                  height: 215,
+                  backgroundColor: 'white',
+                  borderTopColor: 'grey',
+                  borderTopWidth: 1,
+                  borderBottomColor: 'grey',
+                  borderBottomWidth: 1,
+                }}>
+                <Text
+                  style={{
+                    fontSize: 18,
+                    fontWeight: 'semibold',
+                    color: Colors.cathayGold,
+                    position: 'absolute',
+                    top: 20,
+                    left: 20,
+                  }}>
+                  Cathay Badge
+                </Text>
+                <LinkButton
+                  title="More details →"
+                  onPress={() => navigation.navigate('Calendar')}
+                  styles={{
+                    touch: {
+                      position: 'absolute',
+                      top: 14,
+                      left: 285,
+                      borderRadius: 10,
+                      padding: 10,
+                    },
+                    buttonText: {
+                      color: Colors.cathayGreen,
+                      fontSize: 15,
+                      fontWeight: 'semibold',
+                    },
+                  }}
+                />
 
-                </View>
-
+                <Text
+                  style={{
+                    fontSize: 15,
+                    fontWeight: 'semibold',
+                    color: Colors.black,
+                    position: 'absolute',
+                    top: 60,
+                    left: 20,
+                  }}>
+                  You have earned 3 badges
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: 'semibold',
+                    color: Colors.black,
+                    position: 'absolute',
+                    top: 105,
+                    left: 20,
+                  }}>
+                  Badges for you, Feels good to move!
+                </Text>
               </View>
-              {/* <ImageBackground source={require('../../assets/HomeScreen/HomeScreen_background_1.jpg')} style={{width: 600, height: '100%'}}>
+            </View>
+            {/* <ImageBackground source={require('../../assets/HomeScreen/HomeScreen_background_1.jpg')} style={{width: 600, height: '100%'}}>
              <Text>Test</Text>
               </ImageBackground> */}
-            </ScrollView>
+          </ScrollView>
           <Toast />
         </View>
       </View>
