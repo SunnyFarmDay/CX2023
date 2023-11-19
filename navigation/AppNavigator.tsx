09324bca1233b8a75bcdc14f227896c068803dbd
Twilight0341 <70617436+Twilight0341@users.noreply.github.com>
2023-11-19 10:17:48 +0800

    rm: Remove the Extra File

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,56 +1,47 @@
-import React, { useContext, useEffect } from 'react';
-import {
-    View,
-    StyleSheet,
-    Image,
-    Dimensions,
-} from 'react-native';
+import React, {useContext, useEffect} from 'react';
+import {View, StyleSheet, Image, Dimensions} from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { DataContext } from '../store/Data';
+import {DataContext} from '../store/Data';
 import LoginScreen from '../screens/LoginScreen';
-import RegisterScreen from '../screens/RegisterScreen';
-import SplashScreen from '../screens/SplashScreen';
-import ForgetPwScreen from '../screens/ForgetPwScreen';
 import MainScreen from '../screens/MainScreen.js';
 
-import { NavigationContainer } from '@react-navigation/native'
+import {NavigationContainer} from '@react-navigation/native';
 
 const AppNavigator: React.FC = () => {
-    const data = useContext(DataContext);
+  const data = useContext(DataContext);
 
-    useEffect(() => {
-        console.log(new Date().toLocaleString()+ ": Current Screen is "+ data.getCurrentScreen())
-    }, [data.appScreen]);
-
-    return (
-        <View style={styles.container}>
-            {/* Switch to different screens */}
-            {(() => {
-                switch (data.appScreen) {
-                case 0:
-                    return <LoginScreen/>
-                case 1:
-                    return <MainScreen/>
-                case 2:
-                    return <RegisterScreen/>
-                case 3:
-                    return <ForgetPwScreen/>
-                case 4: 
-                    return <SplashScreen />
-                default:
-                    return null
-                }
-            })()}
-        </View>
+  useEffect(() => {
+    console.log(
+      new Date().toLocaleString() +
+        ': Current Screen is ' +
+        data.getCurrentScreen(),
     );
-}
+  }, [data.appScreen]);
+
+  return (
+    <View style={styles.container}>
+      {/* Switch to different screens */}
+      {(() => {
+        switch (data.appScreen) {
+          case 0:
+            return <LoginScreen />;
+          case 1:
+            return <MainScreen />;
+
+          default:
+            return null;
+        }
+      })()}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-    }
+  container: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
 
 export default AppNavigator;
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,86 +1,117 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { View, Image, StyleSheet, Button, ScrollView, Text, TouchableOpacity } from 'react-native';
-import { DataContext } from '../store/Data';
-import HomeScreen from './main/HomeScreen'
-import CalendarScreen from './main/CalendarScreen'
-import RecordScreen from './main/RecordScreen'
-import TrendScreen from './main/TrendScreen'
-import SettingsScreen from './main/SettingsScreen'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { CalendarIcon, ChartBarIcon, HomeIcon, PlusIcon, WrenchIcon } from 'react-native-heroicons/outline';
-import { StringsContext } from '../store/Strings';
+import React, {useContext, useEffect, useState} from 'react';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Button,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import {DataContext} from '../store/Data';
+import HomeScreen from './main/HomeScreen';
+import CalendarScreen from './main/CalendarScreen';
+import RecordScreen from './main/RecordScreen';
+import TrendScreen from './main/TrendScreen';
+import SettingsScreen from './main/SettingsScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  HomeIcon,
+  PlusIcon,
+  WrenchIcon,
+} from 'react-native-heroicons/outline';
+import {StringsContext} from '../store/Strings';
 import SideBar from '../components/SideBar';
 
 const BottomTab = createBottomTabNavigator();
 
 function CustomTabBarButton({children, onPress}) {
-  
-  return(
+  return (
     <TouchableOpacity
-    style={{
-      top: -15,
-      justifyContent: 'center',
-      alignItems: 'center',
-      ...styles.shadow
-    }}
-    onPress={onPress}>
-  
-      <View style={{
-        width: 80,
-        height: 80,
-        borderRadius: 35,
-        backgroundColor: 'rgb(38,92,98)'
-      }}>
+      style={{
+        top: -15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...styles.shadow,
+      }}
+      onPress={onPress}>
+      <View
+        style={{
+          width: 80,
+          height: 80,
+          borderRadius: 35,
+          backgroundColor: 'rgb(38,92,98)',
+        }}>
         {children}
       </View>
-  
     </TouchableOpacity>
-  )
+  );
 }
 
 const MainScreen = () => {
+  const Str = useContext(StringsContext);
+  const setOpenSideBar = useContext(DataContext).setOpenSideBar;
+  const openSideBar = useContext(DataContext).openSideBar;
 
-  const Str = useContext(StringsContext)
-  const setOpenSideBar = useContext(DataContext).setOpenSideBar
-  const openSideBar = useContext(DataContext).openSideBar
-        
-    return (
-        <View style={{flex: 1, position: 'relative'}}>
-        {openSideBar ? <SideBar/> : null}
-        
-        <BottomTab.Navigator screenOptions={{
+  return (
+    <View style={{flex: 1, position: 'relative'}}>
+      {openSideBar ? <SideBar /> : null}
+
+      <BottomTab.Navigator
+        screenOptions={{
           tabBarShowLabel: false,
           tabBarStyle: {
             position: 'absolute',
             bottom: 25,
-            left: 20, 
+            left: 20,
             right: 20,
             backgroundColor: 'white',
             borderRadius: 15,
             height: 90,
-            display: "none",
-            ...styles.shadow
-          }
+            display: 'none',
+            ...styles.shadow,
+          },
         }}>
-          <BottomTab.Screen name="Home" component={HomeScreen} options={{
-            headerShown: false
-          }}/>
-          <BottomTab.Screen name="Calendar" component={CalendarScreen} options={{
-            headerShown: false,
-          }}/>
-          <BottomTab.Screen name="Record" component={RecordScreen} 
+        <BottomTab.Screen
+          name="Home"
+          component={HomeScreen}
           options={{
-            headerShown: false}}
-          />
-          <BottomTab.Screen name="Trend" component={TrendScreen} options={{
             headerShown: false,
-          }}/>
-          <BottomTab.Screen name="Settings" component={SettingsScreen} options={{
+          }}
+        />
+        <BottomTab.Screen
+          name="Calendar"
+          component={CalendarScreen}
+          options={{
             headerShown: false,
-          }}/>
-        </BottomTab.Navigator>
-        </View>
-    );
+          }}
+        />
+        <BottomTab.Screen
+          name="Record"
+          component={RecordScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <BottomTab.Screen
+          name="Trend"
+          component={TrendScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <BottomTab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </BottomTab.Navigator>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -90,8 +121,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  shadow :{
-    shadowColor: "grey",
+  shadow: {
+    shadowColor: 'grey',
     shadowOffset: {
       width: 0,
       height: 10,
@@ -99,7 +130,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.5,
     shadowRadius: 3.5,
     elevation: 5,
-  }
+  },
 });
 
 export default MainScreen;
